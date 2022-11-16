56f83753572c8d9f8ff081f9c08154735e7fbfa6
Omprakash <omg09080@gmail.com>
2022-11-16 12:02:16 +0530

    improved ui

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react"
 import "./header.css"
 import { nav } from "../../data/Data"
 import { Link } from "react-router-dom"
-import { AiOutlineHeart} from 'react-icons/ai'
+import { AiOutlineHeart,AiOutlineClose} from 'react-icons/ai'
+import {MdMenu} from 'react-icons/md'
 
 const Header = ({favs}) => {
   const [navList, setNavList] = useState(false)
@@ -14,30 +15,33 @@ const Header = ({favs}) => {
       <header>
         <div className='container flex'>
           <div className='logo'>
-            {/* <img src='./images/logo.png' alt='' /> */}
             <p className="bold gr">Estatery</p>
           </div>
           <div className='nav'>
             <ul className={navList ? "small" : "flex"}>
               {nav.map((list, index) => (
-                <li key={index}>
+               <li key={index}>
                   <Link to={list.path}>{list.text}</Link>
                 </li>
               ))}
-            </ul>
-          </div>
-          <div className='button flex'>
-            <Link to="/favs" className="favs">
+              <li>
+               <Link to="/favs" className="favs">
               <AiOutlineHeart size={30}/>
               <p className="fav-nums">{favs.length}</p>
             </Link>
+            </li>
+
+            </ul>
+          </div>
+          <div className='button flex'>
+           
             <button className='btn1'>
               <i className='fa fa-sign-out'></i> Sign In
             </button>
           </div>
 
           <div className='toggle'>
-            <button onClick={() => setNavList(!navList)}>{navList ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}</button>
+            <button onClick={() => setNavList(!navList)}>{!navList ? <MdMenu className="menu" size={30} color="white"/> : <AiOutlineClose className="menu" size={25} color="white"/>}</button>
           </div>
         </div>
       </header>
